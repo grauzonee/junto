69d7e1c9cb4415f4eadaa07d6ee2dd7ed70c6d5a
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-09-11 12:02:46 +0200

    Implement schema middleware + tests

--- app/src/routes/event.ts
+++ app/src/routes/event.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { create, list } from "@/controllers/eventController"
+import { requestSchemaValidate } from "@/middlewares/requestSchemaValidate";
+import { CreateEventSchema } from "@/schemas/http/Event";
 
 export const router = Router()
 
-router.post('/', create)
+router.post('/', requestSchemaValidate(CreateEventSchema), create)
 router.get('/', list)
