b271e9b81aa0ab0c20f5b3cbbf96996031542097
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-09-24 12:00:57 +0200

    implement password repeat

--- app/src/controllers/eventController.ts
+++ app/src/controllers/eventController.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express"
 import { addEvent } from "@/queues/createEventQueue"
 import { logger } from "@/config/loggerConfig";
-import { getRepository, geoSearch, textSearch } from "@/schemas/redis/Event"
+import { getRepository } from "@/schemas/redis/Event"
 import type { IEvent } from "@/models/Event";
-import { CoordinatesSchema } from "@/schemas/http/Event";
 
 export async function create(req: Request, res: Response) {
     const repo = await getRepository()
@@ -28,28 +27,6 @@ export async function create(req: Request, res: Response) {
 
 export async function list(req: Request, res: Response) {
     const repo = await getRepository()
-    const data = await repo.search().return.page(req.offset ?? 0, req.limit ?? 20)
+    const data = await repo.search().return.all()
     res.status(200).json({ success: true, data: data })
-}
-
-export async function geosearch(req: Request, res: Response) {
-    const { error, value } = CoordinatesSchema.validate(req.query)
-    if (error) {
-        res.status(400).json({ success: false, message: error })
-        return
-    }
-    const result = await geoSearch(value.lat, value.lon, value.radius, req.offset, req.limit);
-
-    res.status(200).json({ success: true, data: result })
-}
-
-export async function search(req: Request, res: Response) {
-    const { q } = req.query
-    if (!q || typeof (q) != 'string' || q.length < 2) {
-        res.status(400).json({ success: false, message: "Query is required" })
-        return
-    }
-    const result = await textSearch(q, req.offset, req.limit);
-
-    res.status(200).json({ success: true, data: result })
 }
