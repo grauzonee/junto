748b811dac2657bfbe85c7bb1b88039f99929a20
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-09-24 13:37:12 +0200

    Fixed update profile http schema

--- app/src/schemas/http/Profile.ts
+++ app/src/schemas/http/Profile.ts
@@ -3,14 +3,8 @@ import { passwordRule } from '@/schemas/http/Auth';
 
 export const UpdateProfileSchema: Joi.Schema = Joi.object({
     username: Joi.string(),
-    name: Joi.string(),
-    surname: Joi.string(),
-    city: Joi.string(),
-    country: Joi.string(),
-    birthdate: Joi.date(),
-    avatar: Joi.string(),
-    description: Joi.string(),
-    materials: Joi.array()
+    avatarUrl: Joi.string(),
+    interests: Joi.array()
 }).min(1);
 
 
