e0165f277fbcb1120ef8f1084bd48e1a6c4c2e50
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-11-21 20:01:23 +0100

    Made Interest index case-insensitive

--- app/src/models/Interest.ts
+++ app/src/models/Interest.ts
@@ -3,41 +3,44 @@ import { type PaginateQueryHelpers, paginate } from "@/helpers/queryHelper";
 import { type Filterable, type FilterableField } from "@/types/Filter";
 
 export interface IInterest {
-  title: string;
+    title: string;
 }
 
 export type InterestDocument = IInterest & Document;
 
 interface InterestModel
-  extends Model<InterestDocument, PaginateQueryHelpers<InterestDocument>>,
-    Filterable {}
+    extends Model<InterestDocument, PaginateQueryHelpers<InterestDocument>>,
+    Filterable { }
 
 export const InterestSchema = new Schema<
-  InterestDocument,
-  InterestModel,
-  object,
-  PaginateQueryHelpers<InterestDocument>
+    InterestDocument,
+    InterestModel,
+    object,
+    PaginateQueryHelpers<InterestDocument>
 >({
-  title: String,
-});
+    title: String,
+},
+    {
+        collation: { locale: 'en', strength: 2 }
+    });
 
 InterestSchema.set("toJSON", {
-  transform: (_doc, ret: Partial<InterestDocument>) => {
-    ret.id = ret._id;
-    delete ret._id;
-    if ("__v" in ret) {
-      delete ret.__v;
-    }
-  },
+    transform: (_doc, ret: Partial<InterestDocument>) => {
+        ret.id = ret._id;
+        delete ret._id;
+        if ("__v" in ret) {
+            delete ret.__v;
+        }
+    },
 });
 
-InterestSchema.statics.getFilterableFields = function (): FilterableField[] {
-  return [{ field: "title", options: "i" }];
+InterestSchema.statics.getFilterableFields = function(): FilterableField[] {
+    return [{ field: "title", options: "i" }];
 };
 
 InterestSchema.query.paginate = paginate;
 
 export const Interest = mongoose.model<InterestDocument, InterestModel>(
-  "Interest",
-  InterestSchema
+    "Interest",
+    InterestSchema
 );
