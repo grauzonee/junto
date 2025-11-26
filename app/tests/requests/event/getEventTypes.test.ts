34241f99a703b314e80cfc36f768655792d0bb0f
Dumm Devka <dummdevka@Dumms-MacBook-Air.local>
2025-11-26 14:11:56 +0100

    WIP: Implemented tests for getEventTypes request handler

---
+++ app/tests/requests/event/getEventTypes.test.ts
@@ -0,0 +1,43 @@
+import { getEventTypes } from "@/requests/event/getEventTypes";
+import { Request, Response } from "express";
+import { Types } from "mongoose"
+
+const req = { body: {}, limit: 0, dbFilter: {} };
+const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+};
+
+describe("getEventTypes request handler tests", () => {
+    it("Should return '_id' and 'title' fields", async () => {
+        await getEventTypes(req as Request, res as Response);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({
+            data: expect.arrayContaining([
+                expect.objectContaining({
+                    "title": expect.any(String),
+                    "_id": expect.any(Types.ObjectId),
+                })
+            ]),
+            success: true
+        });
+    })
+    it("Should paginate", async () => {
+        req.limit = 2;
+        await getEventTypes(req as Request, res as Response);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({
+            data: [
+                expect.objectContaining({
+                    "title": expect.any(String),
+                    "_id": expect.any(Types.ObjectId),
+                }),
+                expect.objectContaining({
+                    "title": expect.any(String),
+                    "_id": expect.any(Types.ObjectId),
+                }),
+            ],
+            success: true
+        });
+    })
+})
