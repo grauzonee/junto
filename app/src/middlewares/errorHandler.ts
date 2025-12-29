568b0cc8aa483fee310d5b323bbda2cdd56581f4
tatiana.goloviznina <tatiana.goloviznina@kununu.com>
2025-12-29 09:14:20 +0100

    Implemented errorHandler and added it to app

---
+++ app/src/middlewares/errorHandler.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { NotFoundError } from "@/types/errors/InputError";
+import mongoose from "mongoose";
+import messages from "@/constants/errorMessages"
+import { ZodError } from "zod";
+import * as z from "zod"
+import { BadInputError } from "@/types/errors/InputError";
+import { parseMongooseValidationError, setErrorResponse } from "@/helpers/requestHelper";
+
+export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+    if (err instanceof NotFoundError) {
+        setErrorResponse(res, 404, {}, [err.message]);
+        return;
+    }
+    if (err instanceof BadInputError) {
+        setErrorResponse(res, 400, { [err.field]: err.message }, []);
+        return;
+    }
+    if (err instanceof mongoose.Error.ValidationError) {
+        const fieldErrors = parseMongooseValidationError(err);
+        setErrorResponse(res, 400, fieldErrors);
+        return;
+    }
+    if (err instanceof ZodError) {
+        const flattenedError = z.flattenError(err);
+        setErrorResponse(res, 400, flattenedError.fieldErrors, flattenedError.formErrors);
+        return;
+    }
+    setErrorResponse(res, 500, {}, [messages.response.SERVER_ERROR()]);
+}
