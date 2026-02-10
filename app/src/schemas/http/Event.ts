4ebee8973b7594ea95d7b5f6443fec8c909847b8
tatiana.goloviznina <tatiana.goloviznina@kununu.com>
2026-02-10 11:03:21 +0100

    Add tests for Event http schema

--- app/src/schemas/http/Event.ts
+++ app/src/schemas/http/Event.ts
@@ -3,9 +3,15 @@ import { Types } from "mongoose"
 import messages from "@/constants/errorMessages"
 
 export const CoordinatesSchema = z.object({
-    lat: z.coerce.number().min(-90).max(90),
-    lng: z.coerce.number().min(-180).max(180),
-    radius: z.number().min(1).max(15).default(3)
+    lat: z.coerce.number()
+        .min(-90, { message: messages.http.MIN("Latitude", -90) })
+        .max(90, { message: messages.http.MAX("Latitude", 90) }),
+    lng: z.coerce.number()
+        .min(-180, { message: messages.http.MIN("Longitude", -180) })
+        .max(180, { message: messages.http.MAX("Longitude", 180) }),
+    radius: z.number().min(1)
+        .max(15, { message: messages.http.MAX("Radius", 15) })
+        .default(3)
 })
 
 export const CreateEventSchema = z.object({
@@ -18,8 +24,15 @@ export const CreateEventSchema = z.object({
     ),
     fullAddress: z.string(),
     location: z.object({
-        type: z.string(),
-        coordinates: z.array(z.float64())
+        type: z.string()
+            .refine(value => value === "Point", { message: messages.validation.NOT_CORRECT("Location type") }),
+        coordinates: z.array(z.float64()).length(2).refine(
+            (coords) => {
+                const [lat, lng] = coords;
+                return lat >= -90 && lat <= 90 && lng >= -180 && lng <= 180;
+            },
+            { message: messages.validation.NOT_CORRECT("Coordinates") }
+        )
     }),
     categories: z.array(z.string()).refine(
         items => new Set(items).size === items.length,
@@ -42,7 +55,9 @@ export const CreateEventSchema = z.object({
         amount: z.number(),
         currency: z.string()
     }).optional(),
-    maxAttendees: z.number().min(1).max(200)
+    maxAttendees: z.number()
+        .min(1, { message: messages.http.MIN("Max Attendees", 1) })
+        .max(200, { message: messages.http.MAX("Max Attendees", 200) })
 }).strict()
 
 export const EditEventSchema = CreateEventSchema.partial().strict();
