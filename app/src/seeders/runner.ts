a63082dfed2285f5c3233f39c8473b19f2224267
tatiana.goloviznina <tatiana.goloviznina@kununu.com>
2026-02-10 10:36:35 +0100

    Add seed all option

--- app/src/seeders/runner.ts
+++ app/src/seeders/runner.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { connectToMongo } from "@/config/mongoConfig";
+import { ca } from 'zod/v4/locales';
 
 async function run() {
   await connectToMongo();
@@ -11,16 +12,35 @@ async function run() {
     console.error("Please specify a seeder name!");
     process.exit(1);
   }
+  if (seederName === "all") {
+    const seeders = ["categories", "interests", "users", "eventtypes", "events"];
+    for (const name of seeders) {
+      try {
+        await runSeeder(name);
+      } catch (err) {
+        process.exit(1);
+      }
 
+    }
+    process.exit(0);
+  }
   try {
-    const { seed } = await import(`./${seederName}.seeder`);
-    await seed();
+    await runSeeder(seederName);
   } catch (err) {
-    // eslint-disable-next-line
-    console.error("Seeder not found or failed:", err);
+    process.exit(1);
   }
 
   process.exit(0);
 }
 
+async function runSeeder(seederName: string) {
+  try {
+    const { seed } = await import(`./${seederName}.seeder`);
+    await seed();
+  } catch (err) {
+    // eslint-disable-next-line
+    console.error(`Seeder ${seederName} not found or failed:`, err);
+    throw err;
+  }
+}
 run();
