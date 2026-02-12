5563fdfb184c81c41d91fc4dc5a969cc11248289
tatiana.goloviznina <tatiana.goloviznina@kununu.com>
2026-02-12 14:25:53 +0100

    Add http test for category endpoint

---
+++ app/tests/requests/categories.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+import app from "@/app";
+import { createUser } from "../generators/user";
+
+let user: Awaited<ReturnType<typeof createUser>>;
+
+jest.mock("@/middlewares/authMiddleware", () => ({
+    authMiddleware: jest.fn(
+        async (req: any, _res: any, next: any) => {
+            req.user = { _id: user._id };
+            next();
+        }
+    ),
+}));
+
+beforeAll(async () => {
+    user = await createUser({}, true);
+    if (!user) {
+        throw new Error("No user could be created, check your seeders");
+    }
+});
+
+describe("GET /api/categories", () => {
+    it("Should return a list of categories", async () => {
+        const res = await request(app).get('/api/categories');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toHaveProperty('data');
+        expect(Array.isArray(res.body.data)).toBe(true);
+        const category = res.body.data[0];
+        expect(category).toHaveProperty('_id');
+        expect(category).toHaveProperty('title');
+        expect(category).toHaveProperty('subcategories');
+        expect(Array.isArray(category.subcategories)).toBe(true);
+        if (category.subcategories.length > 0) {
+            const subcategory = category.subcategories[0];
+            expect(subcategory).toHaveProperty('_id');
+            expect(subcategory).toHaveProperty('title');
+        }
+    });
+});
